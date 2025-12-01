8fb4d43a4ddab592ecd6083013aad1b14adae6a6
Andrew McGivery <andrew@mcgivery.com>
2025-12-01 15:50:45 -0800

    Add unit test for useSendFollowUpMessage

---
+++ src/hooks/useSendFollowUpMessage.test.ts
@@ -0,0 +1,13 @@
+import { expect, test, vi } from "vitest";
+import { useSendFollowUpMessage } from "./useSendFollowUpMessage";
+
+test("Should set display mode when returned function is called", async () => {
+  vi.stubGlobal("openai", {
+    sendFollowUpMessage: vi.fn(async (args: { prompt: string }) => {}),
+  });
+
+  const sendFollowUpMessage = useSendFollowUpMessage();
+  await sendFollowUpMessage("Do a cool thing!");
+
+  expect(window.openai.sendFollowUpMessage).toBeCalledWith({ prompt: "Do a cool thing!" });
+});
