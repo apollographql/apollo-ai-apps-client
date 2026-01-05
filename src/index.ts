269d3cf1f5f916cd84909c86b9cbf36a46299f8c
Jerel Miller <jerelmiller@gmail.com>
2026-01-05 16:02:22 -0700

    Explictly list exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-export * from "./types/openai";
-export * from "./types/application-manifest";
-export * from "./react/hooks/useOpenAiGlobal";
-export * from "./react/hooks/useToolName";
-export * from "./react/hooks/useToolInput";
-export * from "./react/hooks/useSendFollowUpMessage";
-export * from "./react/hooks/useRequestDisplayMode";
-export * from "./react/hooks/useToolEffect";
+export type {
+  API,
+  CallTool,
+  DeviceType,
+  DisplayMode,
+  OpenAiGlobals,
+  SafeArea,
+  SafeAreaInsets,
+  Theme,
+  UserAgent,
+  UnknownObject,
+} from "./types/openai";
+export { SET_GLOBALS_EVENT_TYPE, SetGlobalsEvent } from "./types/openai";
+
+export type {
+  ApplicationManifest,
+  ManifestOperation,
+  ManifestTool,
+  ManifestExtraInput,
+  ManifestCsp,
+} from "./types/application-manifest";
+
+export { ToolUseProvider } from "./react/context/ToolUseContext";
+export { useOpenAiGlobal } from "./react/hooks/useOpenAiGlobal";
+export { useToolName } from "./react/hooks/useToolName";
+export { useToolInput } from "./react/hooks/useToolInput";
+export { useSendFollowUpMessage } from "./react/hooks/useSendFollowUpMessage";
+export { useRequestDisplayMode } from "./react/hooks/useRequestDisplayMode";
+export { useToolEffect } from "./react/hooks/useToolEffect";
 export { useOpenExternal } from "./react/hooks/useOpenExternal";
 export { useToolOutput } from "./react/hooks/useToolOutput";
 export { useToolResponseMetadata } from "./react/hooks/useToolResponseMetadata";
