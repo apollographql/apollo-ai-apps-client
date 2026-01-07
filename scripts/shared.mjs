ad901be831b28c194532105d979677d1ec338d4b
Jerel Miller <jerelmiller@gmail.com>
2026-01-06 19:10:41 -0700

    Remove old build scripts

--- scripts/shared.mjs
+++
@@ -1,9 +0,0 @@
-export const sharedConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  outdir: "dist",
-  platform: "browser",
-  format: "esm",
-  packages: "external",
-  external: ["react", "react-dom"],
-};
