cfec54a78db05972b135594076cf2f15b4cadd2e
Jerel Miller <jerelmiller@gmail.com>
2026-01-26 16:43:42 -0700

    Add helper to mock application manifest

--- src/testing/internal/index.ts
+++ src/testing/internal/index.ts
@@ -1,2 +1,3 @@
 export { dispatchStateChange } from "./openai/dispatchStateChange.js";
+export { mockApplicationManifest } from "./utilities/mockApplicationManifest.js";
 export { stubOpenAiGlobals } from "./openai/stubOpenAiGlobals.js";
