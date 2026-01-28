d97bccd58c07377428e598d78ed5a6f691a29c87
Jerel Miller <jerelmiller@gmail.com>
2026-01-28 12:25:52 -0700

    Add basic ApolloClient for MCP app

---
+++ src/mcp/core/ApolloClient.ts
@@ -0,0 +1,130 @@
+import { ApolloLink } from "@apollo/client";
+import { ApolloClient as BaseApolloClient } from "@apollo/client";
+import { DocumentTransform } from "@apollo/client";
+import { removeDirectivesFromDocument } from "@apollo/client/utilities/internal";
+import { parse } from "graphql";
+import { __DEV__ } from "@apollo/client/utilities/environment";
+import type { ApplicationManifest } from "../../types/application-manifest.js";
+import { ToolCallLink } from "../link/ToolCallLink.js";
+import { aiClientSymbol, cacheAsync } from "../../utilities/index.js";
+import { McpApp } from "./McpApp.js";
+
+export declare namespace ApolloClient {
+  // This allows us to extend the options with the "manifest" option AND make link optional (it is normally required)
+  export interface Options extends Omit<BaseApolloClient.Options, "link"> {
+    link?: BaseApolloClient.Options["link"];
+    manifest: ApplicationManifest;
+  }
+}
+
+export class ApolloClient extends BaseApolloClient {
+  manifest: ApplicationManifest;
+  readonly app: McpApp;
+
+  protected readonly info = aiClientSymbol;
+
+  constructor(options: ApolloClient.Options) {
+    const link = options.link ?? new ToolCallLink();
+
+    if (__DEV__) {
+      validateTerminatingLink(link);
+    }
+
+    super({
+      ...options,
+      link,
+      // Strip out the prefetch/tool directives so they don't get sent with the operation to the server
+      documentTransform: new DocumentTransform((document) => {
+        return removeDirectivesFromDocument(
+          [{ name: "prefetch" }, { name: "tool" }],
+          document
+        )!;
+      }),
+    });
+
+    this.manifest = options.manifest;
+    this.app = new McpApp(this.manifest);
+  }
+
+  waitForInitialization = cacheAsync(async () => {
+    await this.app.connect();
+
+    const waitForToolOutput = async (): Promise<{
+      prefetch?: Record<string, ApolloLink.Result<any>>;
+    } | null> => {
+      if (this.app.toolResult) {
+        return Promise.resolve(this.app.toolResult);
+      }
+
+      return new Promise((resolve) => {
+        const unsubscribe = this.app.onChange(
+          "toolResult",
+          ({ structuredContent }) => {
+            resolve(structuredContent ?? null);
+            unsubscribe();
+          }
+        );
+      });
+    };
+
+    const toolOutput = await waitForToolOutput();
+
+    if (!toolOutput) {
+      return;
+    }
+
+    // Write prefetched data to the cache
+    this.manifest.operations.forEach((operation) => {
+      if (
+        operation.prefetchID &&
+        toolOutput?.prefetch?.[operation.prefetchID]
+      ) {
+        this.writeQuery({
+          query: parse(operation.body),
+          data: toolOutput.prefetch[operation.prefetchID].data,
+        });
+      }
+
+      // If this operation has the tool that matches up with the tool that was executed, write the tool result to the cache
+      // if (
+      //   operation.tools?.find(
+      //     (tool) =>
+      //       `${this.manifest.name}--${tool.name}` ===
+      //       window.openai.toolResponseMetadata?.toolName
+      //   )
+      // ) {
+      //   // We need to include the variables that were used as part of the tool call so that we get a proper cache entry
+      //   // However, we only want to include toolInput's that were graphql operation (ignore extraInputs)
+      //   const variables = Object.keys(window.openai.toolInput).reduce(
+      //     (obj, key) =>
+      //       operation.variables?.[key] ?
+      //         { ...obj, [key]: window.openai.toolInput[key] }
+      //       : obj,
+      //     {}
+      //   );
+      //
+      //   if (window.openai.toolOutput) {
+      //     this.writeQuery({
+      //       query: parse(operation.body),
+      //       data: (window.openai.toolOutput.result as any).data,
+      //       variables,
+      //     });
+      //   }
+      // }
+    });
+  });
+}
+
+function validateTerminatingLink(link: ApolloLink) {
+  let terminatingLink = link;
+
+  while (terminatingLink.right) {
+    terminatingLink = terminatingLink.right;
+  }
+
+  if (terminatingLink.constructor.name !== "ToolCallLink") {
+    throw new Error(
+      "The terminating link must be a `ToolCallLink`. If you are using a `split` link, ensure the `right` branch uses a `ToolCallLink` as the terminating link."
+    );
+  }
+}
---
+++ src/mcp/core/McpApp.ts
@@ -0,0 +1,74 @@
+import { App, PostMessageTransport } from "@modelcontextprotocol/ext-apps";
+import type { ApplicationManifest } from "../../types/application-manifest";
+
+type State = Record<"toolResult" | "toolInput", unknown>;
+
+export class McpApp {
+  private app: App;
+  private state: State = { toolResult: undefined, toolInput: undefined };
+  private handlers = new Map<keyof State, Set<(...args: any[]) => any>>();
+
+  constructor(manifest: ApplicationManifest) {
+    // TODO: Determine how we want to provide this version long-term.
+    this.app = new App({ name: manifest.name, version: "1.0.0" });
+    this.registerListeners();
+
+    this.callServerTool = this.app.callServerTool.bind(this.app);
+  }
+
+  get toolResult() {
+    return this.state.toolResult;
+  }
+
+  get toolInput() {
+    return this.state.toolInput;
+  }
+
+  connect() {
+    try {
+      return this.app.connect(
+        new PostMessageTransport(window.parent, window.parent)
+      );
+    } catch (e) {
+      const error =
+        e instanceof Error ? e : new Error("Failed to connect", { cause: e });
+
+      throw error;
+    }
+  }
+
+  callServerTool: App["callServerTool"];
+
+  onChange<Key extends keyof State>(name: Key, cb: App[`on${Lowercase<Key>}`]) {
+    let listeners = this.handlers.get(name);
+
+    if (!listeners) {
+      this.handlers.set(name, (listeners = new Set()));
+    }
+
+    listeners.add(cb);
+
+    return () => {
+      listeners.delete(cb);
+    };
+  }
+
+  private registerListeners() {
+    this.app.ontoolresult = (params) => {
+      this.set("toolResult", params);
+    };
+
+    this.app.ontoolinput = (params) => {
+      this.set("toolInput", params);
+    };
+  }
+
+  private set(key: keyof State, value: unknown) {
+    this.state[key] = value;
+    this.notify(key);
+  }
+
+  private notify(key: keyof State) {
+    this.handlers.get(key)?.forEach((listener) => listener(this.state[key]));
+  }
+}
